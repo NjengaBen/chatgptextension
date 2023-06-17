2493047a89ec91b18c78203ce34ce9f051076a84
NjengaBen <bennjenga2@gmail.com>
2023-06-17 19:48:22 +0300

    more updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import { OpenAIApi, Configuration } from "openai";
+import { config } from "dotenv";
+config();
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log(process.env.OPENAI_API_KEY);
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+  openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "Hello world" }],
+    })
+    .then((res) => {
+      console.log(res);
+    });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="w-[320px] h-[540px] bg-white border border-gray-400 rounded-lg">
+      <h1 className="bg-green-500 text-white text-4xl text-center font-semibold rounded-t-lg">
+        Chatgpt
+      </h1>
+      <div className="flex flex-col gap-3 m-3">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          className="bg-gray-100 w-full"
+        ></textarea>
+        <button className="bg-green-500 text-white text-2xl px-1 py-3">
+          Generate
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="m-3">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          className="bg-gray-100 w-full"
+        ></textarea>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
